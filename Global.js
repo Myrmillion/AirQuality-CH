27ec58069853170cad78ed7310a1507cebeaf571
Guillaume 'Myrmillion' D <>
2022-02-14 17:44:47 +0100

    save as of 14.02.2022, next : - gotta retrieve SHORT_NAMES - gotta retrieve FULL_NAMES - gotta retrieve UNITS (better)

--- Global.js
+++ Global.js
@@ -10,12 +10,15 @@
 
 let PATH_TO_DB = 'res/db/'
 
+let DB_FILES = ['O3', 'NO2', 'SO2', 'CO', 'NMVOC', 'EC', 'CPC', 'TEMP', 'RAD', 'PREC', 'PM10', 'PM2.5', 'NOX']
+
 let STATIONS_NAMES = ['Bern-Bollwerk', 'Lausanne-Cesar-Roux', 'Lugano-Universita', 'Zurich-Kaserne',
     'Basel-Binningen', 'Dubendorf-Empa', 'Harkingen-A1', 'Sion-Aeroport-A9', 'Magadino-Cadenazzo',
     'Payerne', 'Tanikon', 'Beromunster', 'Chaumont', 'Rigi-Seebodenalp', 'Davos-Seehornwald', 'Jungfraujoch']
 
 let INFOS = {
-    NAMES: ['O3', 'NO2', 'SO2', 'CO', 'NMVOC', 'EC', 'CPC', 'TEMP', 'RAD', 'PREC', 'PM10', 'PM25', 'NOX'],
+    SHORT_NAME: FILL_INFOS_NAMES,
+    LONG_NAME:
     UNITS: []
 }
 
@@ -24,6 +27,17 @@ FILL_INFOS_UNITS();
 //------------------------------------------------------------------//
 //                           PREPARATION                            //
 //------------------------------------------------------------------//
+
+function FILL_INFOS_NAMES() {
+
+    let array = []
+
+    for (let infoName of DB_FILES) {
+        array.push(infoName.replace(/\./g,''));
+    }
+
+
+}
 
 function FILL_INFOS_UNITS() {
 
